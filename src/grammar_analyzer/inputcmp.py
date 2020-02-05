822af073251de1d108b4a065206d959a8118d68f
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2020-02-05 17:48:12 -0500

    Add input grammar and ast

---
+++ src/grammar_analyzer/inputcmp.py
@@ -0,0 +1,54 @@
+from pycmp.grammar import Grammar
+from pycmp.ast import Node
+
+
+class GNode(object):
+    def __init__(self, productions):
+        self.productions = tuple(productions)
+
+
+class ProdNode(object):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = tuple(right)
+
+
+class SentNode(object):
+    def __init__(self, symbols):
+        self.symbols = tuple(symbols)
+
+
+class SymbolNode(object):
+    def __init__(self, lex):
+        self.lex = lex
+
+
+def build_input_grammar():
+    """
+    Returns the following grammar:
+
+    grammar -> prod_list
+    prod_list -> prod | prod prod_list
+    prod -> symbol '->' sent_list
+    sent_list -> sent | sent '|' sent_list
+    sent -> symbol_list | 'eps'
+    symbol_list -> symbol | symbol symbol_list
+    """
+    input_grammar = Grammar()
+    grammar = input_grammar.add_nonterminal('grammar', True)
+    prod, prod_list = input_grammar.add_nonterminals('prod prod_list')
+    sent, sent_list, symbol_list = input_grammar.add_nonterminals('sent sent_list symbol_list')
+    symbol, arrow, union, eps = input_grammar.add_terminals('symbol -> | eps')
+
+    grammar %= prod_list, lambda h, s: GNode(s[0])
+    prod_list %= prod + prod_list, lambda h, s: [s[0]] + s[1]
+    prod_list %= prod, lambda h, s: [s[0]]
+    prod %= symbol + arrow + sent_list, lambda h, s: ProdNode(s[0], s[2])
+    sent_list %= sent + union + sent_list, lambda h, s: lambda h, s: [s[0]] + s[2]
+    sent_list %= sent, lambda h, s: [s[0]]
+    sent %= symbol_list, lambda h, s: SentNode(s[0])
+    sent %= eps, lambda h, s: SentNode([s[0]])
+    symbol_list %= symbol + symbol_list, lambda h, s: [s[0]] + s[1]
+    symbol_list %= symbol, lambda h, s: [s[0]]
+
+    return input_grammar
