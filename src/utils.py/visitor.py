5e8722031ee6c2173f3d2007b33a626c9b7f1235
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2020-02-06 09:15:04 -0500

    Add visitor in utils module

---
+++ src/utils.py/visitor.py
@@ -0,0 +1,39 @@
+from inspect import signature
+
+registry = {}
+
+
+class Visitor(object):
+    def __init__(self, params):
+        self.params = params
+        self.typemap = {}
+
+    def __call__(self, *args):
+        types = tuple(args[i].__class__ for i in self.params)
+        function = self.typemap[types]
+        return function(*args)
+
+    def register(self, types, function):
+        assert types not in self.typemap
+        self.typemap[types] = function
+
+    def when(self, *types):
+        assert len(types) == len(self.params)
+
+        def register(function):
+            self.register(types, function)
+            return function
+        return register
+
+
+def on(*argnames):
+    def register(function):
+        name = function.__name__
+        params = signature(function).parameters
+        params = [i for i, _ in enumerate(argnames)]
+
+        assert name not in registry
+        registry[name] = Visitor(params)
+
+        return registry[name]
+    return register
