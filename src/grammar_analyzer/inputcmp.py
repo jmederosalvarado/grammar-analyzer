01c76d82b93a599600505ab627519fecca04f144
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2020-02-06 09:14:43 -0500

    Add lexer

--- src/grammar_analyzer/inputcmp.py
+++ src/grammar_analyzer/inputcmp.py
@@ -1,5 +1,6 @@
 from pycmp.grammar import Grammar
 from pycmp.ast import Node
+from pycmp.lexer import Lexer
 
 
 class GNode(object):
@@ -38,10 +39,10 @@ def build_input_grammar():
     grammar = input_grammar.add_nonterminal('grammar', True)
     prod, prod_list = input_grammar.add_nonterminals('prod prod_list')
     sent, sent_list, symbol_list = input_grammar.add_nonterminals('sent sent_list symbol_list')
-    symbol, arrow, union, eps = input_grammar.add_terminals('symbol -> | eps')
+    symbol, arrow, union, eps, eol = input_grammar.add_terminals('symbol -> | eps eol')
 
     grammar %= prod_list, lambda h, s: GNode(s[0])
-    prod_list %= prod + prod_list, lambda h, s: [s[0]] + s[1]
+    prod_list %= prod + eol + prod_list, lambda h, s: [s[0]] + s[1]
     prod_list %= prod, lambda h, s: [s[0]]
     prod %= symbol + arrow + sent_list, lambda h, s: ProdNode(s[0], s[2])
     sent_list %= sent + union + sent_list, lambda h, s: lambda h, s: [s[0]] + s[2]
@@ -52,3 +53,16 @@ def build_input_grammar():
     symbol_list %= symbol, lambda h, s: [s[0]]
 
     return input_grammar
+
+
+def build_lexer(grammar):
+    digits = '|'.join(str(n) for n in range(10))
+    letters = '|'.join(chr(n) for n in range(ord('a'), ord('z')+1))
+    return Lexer([
+        (grammar['eps'], 'eps'),
+        (grammar['|'], r'\|'),
+        (grammar['->'], '->'),
+        (grammar['eol'], '\\n'),
+        (None, '  *'),
+        (grammar['symbol'], f'({letters}|{digits})*')
+    ], grammar.eof)
