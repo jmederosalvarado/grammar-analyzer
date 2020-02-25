3f7958f32e7922429837bfea709d83dbff1fb29e
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2020-02-24 23:51:52 -0500

    Add lr?analysis

--- src/grammar_analyzer/lr_analyzer.py
+++ src/grammar_analyzer/lr_analyzer.py
@@ -18,13 +18,13 @@ class __LR1ParserConflicts(LR1Parser):
         if key in table and value not in table[key]:
             table[key].append(value)
         else:
-            table[key].append(value)
+            table[key] = [value]
 
 
 @lru_cache
 def __build_lr_info(grammar):
     parser_conflicts = __LR1ParserConflicts(grammar)
-    automaton = build_lr1_automaton(grammar)
+    automaton = build_lr1_automaton(grammar.get_augmented_grammar(True))
     return shift_reduce_info(
         automaton,
         parser_conflicts.action,
