fb53e311de4be27b4eb31b159a3cf61e7e361e66
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2020-03-10 02:43:17 -0400

    Remove some commented code

--- src/grammar_analyzer/lalr_analyzer.py
+++ src/grammar_analyzer/lalr_analyzer.py
@@ -9,10 +9,6 @@ from grammar_analyzer.shift_reduce_analyzer import (
 )
 from grammar_analyzer.common import build_derivation_tree
 
-# TODO: Refactor all shift-reduce analyzers to share common code
-
-# region Carmen's code
-
 
 def merge_items_lookaheads(items, others):
     if len(items) != len(others):
@@ -110,93 +106,6 @@ class LALRParser(ShiftReduceParser):
             key not in table or table[key] == value
         ), "Shift-Reduce or Reduce-Reduce conflict!!!"
         table[key] = value
-
-
-# endregion
-
-# # pylint: disable=function-redefined
-# class LALRParser(ShiftReduceParser):
-#     def _build_parsing_table(self):
-#         """
-#         Method to construct an LALR parser:
-#         1. Construct C = (Io, 11, ... , I,), the collection of sets of LR(1) items.
-#         2. For each core present among the set of LR(1) items, find all sets having
-#            that core, and replace these sets by their union.
-#         3. Let C' = {Jo, J1,... , J,) be the resulting sets of LR(1) items. The
-#            parsing actions for state i are constructed from Ji in the same manner as
-#            in LR. If there is a parsing action conflict, the algorithm fails
-#            to produce a parser, and the grammar is said not to be LALR(1).
-#         4. The GOTO table is constructed as follows. If J is the union of one or
-#            more sets of LR(1) items, that is, J = Il n I2 n ... n Ik, then the
-#            cores of GOTO(I1, X) , GOTO(I2, X) , ... , GOTO(Ik, X) are the same, since
-#            11, 12, ... , Ik all have the same core. Let K be the union of all sets of
-#            items having the same core as GOTO(I1, X). Then GOTO(J, X) = K.
-#         """
-#         grammar = self.grammar.get_augmented_grammar(True)
-
-#         automaton = build_lr1_automaton(grammar)
-#         merged_states = self._merge_states(node.state for node in automaton)
-
-#         for idx, state in enumerate(merged_states):
-#             for item in state:
-#                 if item.is_reduce_item:
-#                     is_start = item.production.left == grammar.start_symbol
-#                     for s in item.lookaheads:
-#                         action = (
-#                             self.OK if is_start and s == grammar.eof else self.REDUCE
-#                         )
-#                         self._register(self.action, (idx, s), (action, item.production))
-#                     continue
-
-#                 x = item.next_symbol
-#                 original_node = next(
-#                     node
-#                     for node in automaton
-#                     if self._get_core(node.state) == self._get_core(state)
-#                 )
-#                 try:
-#                     original_dest = original_node.transitions[x.name][0].state
-#                 except KeyError:
-#                     continue
-#                 dest_idx = next(
-#                     i
-#                     for i, s in enumerate(merged_states)
-#                     if self._get_core(s) == self._get_core(original_dest)
-#                 )
-#                 if x.is_terminal:
-#                     self._register(self.action, (idx, x), (self.SHIFT, dest_idx))
-#                 else:
-#                     self._register(self.goto, (idx, x), dest_idx)
-
-#     @classmethod
-#     def _merge_states(cls, states):
-#         cores, new_states = frozenset(cls._get_core(s) for s in states), []
-#         for core in cores:
-#             items = chain(state for state in states if cls._get_core(state) == core)
-#             new_state = frozenset(
-#                 Item(
-#                     center.production,
-#                     center.pos,
-#                     lookaheads=chain(
-#                         item.lookaheads for item in items if item.center() == center
-#                     ),
-#                 )
-#                 for center in core
-#             )
-#             new_states.append(new_state)
-
-#         return new_states
-
-#     @staticmethod
-#     def _get_core(state):
-#         return frozenset(i.center() for i in state)
-
-#     @staticmethod
-#     def _register(table, key, value):
-#         assert (
-#             key not in table or table[key] == value
-#         ), "Shift-Reduce or Reduce-Reduce conflict!!!"
-#         table[key] = value
 
 
 class __LALRParserConflicts(LALRParser):
